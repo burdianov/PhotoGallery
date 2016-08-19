58559f93a5c9dbf0875b22dd9c817e5b3183b8c8
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-19 20:00:29 +0400

    Fetch website html code

--- app/src/main/java/com/testography/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/testography/photogallery/PhotoGalleryFragment.java
@@ -1,15 +1,21 @@
 package com.testography.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
+    public static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment newInstance() {
@@ -20,6 +26,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -32,5 +39,20 @@ public class PhotoGalleryFragment extends Fragment {
                 3));
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String result = new FlickrFetchr()
+                        .getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
