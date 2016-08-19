4f87bc801ce9f426f72bbe09d65f4b060833e186
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-19 19:24:47 +0400

    Setup new project structure with fragment

---
+++ app/src/main/java/com/testography/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.testography.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+
+}
